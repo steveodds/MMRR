9a200cf1b803d8278e401c94525d9754f03c6827
Stephen Odundo <steveremdi@gmail.com>
2021-04-06 18:20:27 +0300

    Added enum for type of mpesa record

---
+++ Mobile Money Records Reconciliator/Mobile Money Records Reconciliator/Core/Enums/TransactionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Money_Records_Reconciliator.Core.Enums
+{
+    public enum TransactionType
+    {
+        Expense,
+        Income
+    }
+}
