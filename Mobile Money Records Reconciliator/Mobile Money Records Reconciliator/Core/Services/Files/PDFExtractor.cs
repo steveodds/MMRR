16a4ad219272aae7f87bb44729ae53e987178d60
Stephen Odundo <steveremdi@gmail.com>
2021-04-07 23:53:38 +0300

    Initial class for handling the pdf statements

---
+++ Mobile Money Records Reconciliator/Mobile Money Records Reconciliator/Core/Services/Files/PDFExtractor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Money_Records_Reconciliator.Core.Services.Files
+{
+    class PDFExtractor
+    {
+        private Models.PDFStatement pdf;
+        public List<Models.MpesaRecord> MpesaRecords { get; set; }
+
+        public PDFExtractor(Models.PDFStatement pdf)
+        {
+            this.pdf = pdf;
+        }
+
+
+
+    }
+}
