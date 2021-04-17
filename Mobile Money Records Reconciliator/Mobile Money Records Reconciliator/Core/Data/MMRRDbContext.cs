237bdde2828f3b309bc777d9f09688d6cf341344
Stephen Odundo <steveremdi@gmail.com>
2021-04-17 23:16:30 +0300

    Added database context

---
+++ Mobile Money Records Reconciliator/Mobile Money Records Reconciliator/Core/Data/MMRRDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Money_Records_Reconciliator.Core.Data
+{
+    class MMRRDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=mmrr.db");
+
+        public DbSet<Core.Models.PDFStatement> PDFStatements { get; set; }
+        public DbSet<Core.Models.MpesaRecord> MpesaRecords { get; set; }
+        public DbSet<Core.Models.FinalFile> FinalFiles { get; set; }
+    }
+}
