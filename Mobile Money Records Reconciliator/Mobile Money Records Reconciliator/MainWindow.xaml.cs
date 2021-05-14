bcdedc6f5bf50b3c2cd9dd87ab0daa30a28462cb
Stephen Odundo <steveremdi@gmail.com>
2021-05-14 22:02:09 +0300

    Removed scrollviewer, dashboard elements and associated methods

--- Mobile Money Records Reconciliator/Mobile Money Records Reconciliator/MainWindow.xaml.cs
+++ Mobile Money Records Reconciliator/Mobile Money Records Reconciliator/MainWindow.xaml.cs
@@ -30,19 +30,16 @@ namespace Mobile_Money_Records_Reconciliator
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        //Required models
-        private Core.Models.PDFStatement statement;
-        //End
+        
         public MainWindow()
         {
             this.InitializeComponent();
-            statement = new Core.Models.PDFStatement();
         }
 
         private double NavViewCompactModeThresholdWidth { get { return NavView.CompactModeThresholdWidth; } }
         private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
         {
-            ("home", typeof(MainWindow)),
+            ("home", typeof(Dashboard)),
             ("statements", typeof(MpesaStatements)),
         };
 
@@ -53,7 +50,7 @@ namespace Mobile_Money_Records_Reconciliator
             // If navigation occurs on SelectionChanged, this isn't needed.
             // Because we use ItemInvoked to navigate, we need to call Navigate
             // here to load the home page.
-            //NavView_Navigate("home", new Microsoft.UI.Xaml.Media.Animation.EntranceNavigationTransitionInfo());
+            NavView_Navigate("home", new Microsoft.UI.Xaml.Media.Animation.EntranceNavigationTransitionInfo());
 
             // Listen to the window directly so the app responds
             // to accelerator keys regardless of which element has focus.
@@ -195,128 +192,6 @@ namespace Mobile_Money_Records_Reconciliator
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
-        //Core Functionality
-        private void SavePDFDetails(string pdfPath)
-        {
-            //TODO: Generate PDF model
-        }
-
-        private void SaveMultiplePDFs(List<string> pdfPaths)
-        {
-            //TODO: Generate Multiple PDF Models
-        }
-
-        private void PDFDetailsToDB(Core.Models.PDFStatement pdfStatement)
-        {
-            //TODO: Save to DB
-        }
-
-        private async void LoadFile_Click(object sender, RoutedEventArgs e)
-        {
-            var fileDialog = new Windows.Storage.Pickers.FileOpenPicker();
-
-            //Make folder Picker work in Win32
-            IntPtr windowHandle = (App.Current as App).WindowHandle;
-            var initializeWithWindow = fileDialog.As<IInitializeWithWindow>();
-
-            fileDialog.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
-            initializeWithWindow.Initialize(windowHandle);
-            fileDialog.FileTypeFilter.Add(".pdf");
-            var pickedFile = await fileDialog.PickSingleFileAsync();
-            if (pickedFile is not null)
-            {
-                statement.FileName = pickedFile.Name;
-                statement.StatementPath = pickedFile.Path;
-                UpdateFiles(windowHandle);
-            }
-        }
-
-        private async void UpdateFiles(IntPtr windowHandle)
-        {
-            statement.PassKey = await GetPasskey();
-            var extractor = new Core.Services.Files.PDFExtractor(statement);
-            var result = await extractor.ExtractRecordsAsync();
-
-            ContentDialog fileLoadedDialog = new ContentDialog
-            {
-                Title = "File Loaded",
-                Content = $"File '{statement.FileName}' has been successfully loaded!",
-                CloseButtonText = "Ok"
-            };
-
-            if (Windows.Foundation.Metadata.ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
-            {
-                fileLoadedDialog.XamlRoot = MainAppGrid.XamlRoot;
-            }
-            await fileLoadedDialog.ShowAsync();
-
-
-            if (!Docs.IsEnabled)
-            {
-                Docs.IsEnabled = true;
-                Docs.Items.Clear();
-            }
-            Docs.Items.Add(statement.FileName);
-        }
-
-        [ComImport]
-        [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
-        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        public interface IInitializeWithWindow
-        {
-            void Initialize(IntPtr hwnd);
-        }
-
-        private async Task<int> GetPasskey()
-        {
-
-
-            if (Windows.Foundation.Metadata.ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
-            {
-                GetPin.XamlRoot = MainAppGrid.XamlRoot;
-            }
-            var userResponse = await GetPin.ShowAsync();
-            if (userResponse == ContentDialogResult.Primary)
-            {
-                var pin = int.Parse(Passkey.Password.ToString());
-                Passkey.Password = string.Empty;
-                return pin;
-            }
-
-            return -1;
-        }
-        private async void ClearDocumentHistory_Click(object sender, RoutedEventArgs e)
-        {
-            if (Docs.IsEnabled)
-            {
-                ContentDialog fileLoadedDialog = new ContentDialog
-                {
-                    Title = "Clear Document History",
-                    Content = $"Are you sure you want to clear your document history? This action is not reversible!",
-                    PrimaryButtonText = "Yes",
-                    CloseButtonText = "No",
-                    DefaultButton = ContentDialogButton.Close
-                };
-
-                if (Windows.Foundation.Metadata.ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
-                {
-                    fileLoadedDialog.XamlRoot = MainAppGrid.XamlRoot;
-                }
-                var userResponse = await fileLoadedDialog.ShowAsync();
-                if (userResponse == ContentDialogResult.Primary)
-                {
-                    Docs.Items.Clear();
-                    Docs.Items.Add("You have no previous documents...");
-                    Docs.IsEnabled = false;
-                    var finalDialog = new ContentDialog();
-                    finalDialog.Content = "History cleared";
-                    finalDialog.Title = fileLoadedDialog.Title;
-                    finalDialog.CloseButtonText = "Close";
-                    if (Windows.Foundation.Metadata.ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
-                        finalDialog.XamlRoot = MainAppGrid.XamlRoot;
-                    await finalDialog.ShowAsync();
-                }
-            }
-        }
+        
     }
 }
