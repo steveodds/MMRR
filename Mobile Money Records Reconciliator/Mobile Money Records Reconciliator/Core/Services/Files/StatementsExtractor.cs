3ed1ddb9f80739ed66d0de0f6393bf96518b5ffd
Stephen Odundo <steveremdi@gmail.com>
2021-05-16 15:14:49 +0300

    New class that will populate models with transactions and statements data

---
+++ Mobile Money Records Reconciliator/Mobile Money Records Reconciliator/Core/Services/Files/StatementsExtractor.cs
@@ -0,0 +1,38 @@
+﻿using Mobile_Money_Records_Reconciliator.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Money_Records_Reconciliator.Core.Services.Files
+{
+    public class StatementsExtractor
+    {
+        private readonly string _pdfText;
+        private List<Models.MpesaRecord> MpesaRecords { get; set; }
+        public Models.StatementsData FullStatements { get; set; }
+        public StatementsExtractor(string pdfText)
+        {
+            _pdfText = pdfText;
+            MpesaRecords = new();
+            FullStatements = new();
+        }
+
+
+        public List<Models.MpesaRecord> GetMpesaRecords()
+        {
+            if (string.IsNullOrEmpty(_pdfText))
+                throw new ArgumentNullException("The provided text was empty. No statements can be extracted.");
+
+            FullStatements = GenerateStatement();
+
+            return null;
+        }
+
+        private StatementsData GenerateStatement()
+        {
+            return null;
+        }
+    }
+}
